6af160f41a725c647441cd41e8fbfcd8c4edf2c8
Siddhant Prakash <siddhantprakash121@gmail.com>
2023-06-04 21:07:57 +0530

    Brute Force Approach
    Here the TC will in the powers of 3. and few test cases were failing

---
+++ Day1/setMatrixZeroBF.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    //mark whole ith row as -1
+                    markrow(matrix, i);
+                    //mark whole jth col as -1
+                    markcol(matrix, j);
+                }
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(matrix[i][j] == -1){
+                    //make them 0
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+    }
+
+     static void markrow(int[][] matrix, int i){
+        for(int j = 0; j < matrix[0].length; j++){
+            if(matrix[i][j] != 0){
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+     static void markcol(int[][] matrix, int j){
+        for(int i = 0; i < matrix.length; i++){
+            if(matrix[i][j] != 0){
+                matrix[i][j] = -1;
+            }
+        }
+    }
+}
